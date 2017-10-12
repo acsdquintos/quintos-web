70b2284737754071d6935641e55fe99e2c2d34ba
Allan Duarte <allanduarte6@gmail.com>
2017-10-12 16:37:34 -0300

    Commit incial dos arquivos do projeto back-end da quintos.

---
+++ src/main/java/com/quintos/QuintosApplication.java
@@ -0,0 +1,12 @@
+package com.quintos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintosApplication.class, args);
+	}
+}
